b476a815790c99db1228e743b3def2ee8a10a661
Cauã <caualeu2016@gmail.com>
2025-06-28 18:13:15 -0300

    adiciona validação de requisição para geração de QR Code

--- src/routes/qrCode.routes.js
+++ src/routes/qrCode.routes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const qrCodeController = require('../controllers/qrCode.controller');
+const { validateQrCodeRequest } = require('../validators/qrCode.validator');
 
 /**
  * @route   POST /api/generate
  * @desc    Gera um QR Code
  * @access  Public
  */
-router.post('/generate', qrCodeController.generateQrCode);
+router.post('/generate', validateQrCodeRequest, qrCodeController.generateQrCode);
 
 // Exporta o router (index.js)
 module.exports = router;
